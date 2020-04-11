739577e7852f65fa0ee590b1fb05b8455b2474e6
Petyo54 <63457620+Petyo54@users.noreply.github.com>
2020-04-11 10:37:03 +0200

    Añadida clase registro

---
+++ src/clubdepadel_entrega/FXMLRegistroController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clubdepadel_entrega;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author awset
+ */
+public class FXMLRegistroController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
