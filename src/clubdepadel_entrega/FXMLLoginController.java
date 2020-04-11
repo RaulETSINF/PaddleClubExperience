75283a3a077624ab69602d43265ddf88cb3bd2a5
TheDraxESP <43744156+TheDraxESP@users.noreply.github.com>
2020-04-11 12:55:22 +0200

    cambios

--- src/clubdepadel_entrega/FXMLLoginController.java
+++ src/clubdepadel_entrega/FXMLLoginController.java
@@ -6,16 +6,19 @@
 package clubdepadel_entrega;
 
 import DBAcess.ClubDBAccess;
+import java.awt.event.KeyEvent;
 import java.io.IOException;
 import java.lang.reflect.Member;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
+import javafx.event.EventType;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.stage.Modality;
@@ -27,34 +30,58 @@ import javafx.stage.Stage;
  * @author RaulP
  */
 public class FXMLLoginController implements Initializable {
-    
+
     @FXML
     private TextField user_Input;
     @FXML
     private PasswordField password_Input;
+
+    String caracteresConf = "!@#$%^&*()_+={}[]|<>,.`~?\\/:;'-";
+    ClubDBAccess miClub;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        ClubDBAccess miClub = ClubDBAccess.getSingletonClubDBAccess();
+        miClub = ClubDBAccess.getSingletonClubDBAccess();
+
+        user_Input.addEventFilter(javafx.scene.input.KeyEvent.KEY_TYPED, (javafx.scene.input.KeyEvent keyEvent) -> {
+            if (caracteresConf.contains(keyEvent.getCharacter())) {
+                keyEvent.consume();
+            }
+        });
+
+        password_Input.addEventFilter(javafx.scene.input.KeyEvent.KEY_TYPED, (javafx.scene.input.KeyEvent keyEvent) -> {
+            if (caracteresConf.contains(keyEvent.getCharacter())) {
+                keyEvent.consume();
+            }
+        });
     }
-    
 
     @FXML
     private void login(ActionEvent event) {
-        
+        if (miClub.getMemberByCredentials(user_Input.getText(), password_Input.getText()) == null) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Dialogo de confirmación");
+            alert.setHeaderText("Error de Inicio de Sesión");
+            alert.setContentText("Puede que hayas introducido los datos erroneamente, revisalos o crea una cuenta");
+            alert.showAndWait();
+
+        } else {
+            System.out.println("Bienvenido");
+        }
+
     }
 
     @FXML
     private void registrarse(ActionEvent event) throws IOException {
         FXMLLoader miCargador = new FXMLLoader(getClass().getResource("/clubdepadel_entrega/FXMLRegistro.fxml"));
         Parent root = miCargador.load();
-        
+
         FXMLRegistroController controladoRegistro = miCargador.<FXMLRegistroController>getController();
         controladoRegistro.initialize(null, null);
-        
+
         Scene scene = new Scene(root);
         Stage stage = new Stage();
         stage.setScene(scene);
@@ -62,5 +89,5 @@ public class FXMLLoginController implements Initializable {
         stage.initModality(Modality.APPLICATION_MODAL);
         stage.showAndWait();
     }
-    
+
 }
