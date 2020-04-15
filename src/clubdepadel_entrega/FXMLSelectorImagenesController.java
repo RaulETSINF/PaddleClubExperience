554ee5d59614673950ffc1aeac4109d437d1383d
TheDraxESP <43744156+TheDraxESP@users.noreply.github.com>
2020-04-15 15:14:13 +0200

    He cambiat les imatges

--- src/clubdepadel_entrega/FXMLSelectorImagenesController.java
+++ src/clubdepadel_entrega/FXMLSelectorImagenesController.java
@@ -14,6 +14,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.image.Image;
