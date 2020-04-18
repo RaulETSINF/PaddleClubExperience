ecc9cda84aff32be91d2e61e415a3e5d9cf7fb19
TheDraxESP <43744156+TheDraxESP@users.noreply.github.com>
2020-04-18 03:37:14 +0200

    He cambiat les imatges

--- src/clubdepadel_entrega/FXMLReservarPistaController.java
+++ src/clubdepadel_entrega/FXMLReservarPistaController.java
@@ -11,7 +11,25 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.DatePicker;
+import DBAcess.ClubDBAccess;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.Month;
+import java.time.format.DateTimeFormatter;
+import java.util.Observable;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.DateCell;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.util.converter.LocalDateTimeStringConverter;
+import model.Booking;
+import model.Court;
 
 /**
  * FXML Controller class
@@ -23,22 +41,59 @@ public class FXMLReservarPistaController implements Initializable {
     @FXML
     private DatePicker datePicker;
     @FXML
-    private TableView<?> tableViewBooking;
+    private TableView<Booking> tableViewBooking;
+
+    ClubDBAccess clubDBAccess;
+    ObservableList<Booking> observableBookings;
+    @FXML
+    private TableColumn<Booking, Booking> horas;
+
+    LocalDate x = LocalDate.of(2020, Month.MARCH, 19);
+    LocalTime y = LocalTime.of(14, 0);
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
+        clubDBAccess = ClubDBAccess.getSingletonClubDBAccess();
+        observableBookings = FXCollections.observableList(clubDBAccess.getBookings());
+        horas.setCellValueFactory(param -> new SimpleObjectProperty<Booking>(param.getValue()));
+        horas.setCellFactory(v -> {
+            return new TableCell<Booking, Booking>() {
+                @Override
+                protected void updateItem(Booking item, boolean empty) {
+                    super.updateItem(item, empty);
+                    if (item == null || empty) {
+                        setText(null);
+                    } else {
+                        setText(""+item.getFromTime());
+                    }
+                }
+            };
+        });
+        clubDBAccess.getBookings().add(new Booking(LocalDateTime.MIN, LocalDate.MAX, LocalTime.NOON, true, clubDBAccess.getCourts().get(0), clubDBAccess.getMembers().get(0)));
+        tableViewBooking.setItems(observableBookings);
+        datePicker.setDayCellFactory((DatePicker picker) -> {
+            return new DateCell() {
+                @Override
+                public void updateItem(LocalDate date, boolean empty) {
+                    super.updateItem(date, empty);
+                    LocalDate today = LocalDate.now();
+                    setDisable(empty || date.compareTo(today) < 0);
+                }
+            };
+        });
+    }
 
     @FXML
+
     private void inspeccionar(ActionEvent event) {
+        clubDBAccess.getBookings().removeAll(observableBookings);
     }
 
     @FXML
     private void reservar(ActionEvent event) {
     }
-    
+
 }
